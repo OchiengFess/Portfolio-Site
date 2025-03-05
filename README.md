25dd80694bf7d0723700f61740a82538acba0b78
OchiengFess <festusochi13@gmail.com>
2025-03-05 22:18:57 +0300

    Portfolio 2025 --Analytics

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Portfolio-Site
 ![Streamlit App](https://raw.githubusercontent.com/OchiengFess/Portfolio-Site/main/thumbnail.jpg)
+![Streamlit App](https://raw.githubusercontent.com/OchiengFess/Portfolio-Site/main/projects/005-SegChurn/Segmentation_Thumbnail.png)
--- projects/005-SegChurn/SegmentationChurn.py
+++ projects/005-SegChurn/SegmentationChurn.py
@@ -10,7 +10,7 @@ from sklearn.cluster import KMeans
 # Streamlit UI
 st.set_page_config(page_title="Customer Segmentation & Churn Dashboard", layout='centered',initial_sidebar_state='expanded')
 
-st.image(("https://github.com/OchiengFess/Portfolio-Site/tree/main/projects/005-SegChurn/Segmentation_Thumbnail.png"), use_container_width=True)
+st.image(("https://raw.githubusercontent.com/OchiengFess/Portfolio-Site/main/projects/005-SegChurn/Segmentation_Thumbnail.png"), use_container_width=True)
 # Dark theme settings
 
 
